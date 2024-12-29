7dc7e13decd4cbc296cf36985a0aac8583ad94c0
Assia hmr <haimeurassia70@gmail.com>
2024-12-29 12:34:23 +0100

    add dockefiles

--- backend/transaction_service/app/db/base.py
+++ backend/transaction_service/app/db/base.py
@@ -6,5 +6,7 @@ Base = declarative_base()
 
 # Importer tous les modèles ici pour Alembic
 from app.models.announcements import Announcement
-from app.models.transactions import Transaction
+def get_transaction():
+    from app.models.transactions import Transaction  # Importation différée
+    return Transaction
 from app.models.marcket_prices import MarketPrice
