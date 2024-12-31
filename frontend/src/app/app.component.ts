07379198e33c042e357d8c6de5525bf7fa739a62
Niama Aqarial <niama.aqarial@gmail.com>
2024-12-31 12:04:11 +0100

    push login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,17 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from "./home/home.component";
 import { SponsorsComponent } from './sponsors/sponsors.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, SponsorsComponent, AboutComponent],
+  // imports: [RouterOutlet, HeaderComponent, HomeComponent, SponsorsComponent, AboutComponent],
+  // imports: [RouterOutlet, LoginComponent, HeaderComponent],
+  imports: [RouterOutlet, SignupComponent, HeaderComponent,LeftSideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
